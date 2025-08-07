a1c254c29f1c0e42ca05e7e469a5c82e57dbcf10
pageofhcr2 <wiileted23@gmail.com>
2025-08-07 16:00:24 +0300

    Update script.py

--- script.py
+++ script.py
@@ -6,4 +6,4 @@ def index():
     return render_template("index.html")  # you need templates/index.html for this to work
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=10000)
